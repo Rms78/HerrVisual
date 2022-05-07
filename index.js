8627d6cc53637379493abed0b141c8138dcea3b5
Rafael Mata Sánchez <83203074+Rms78@users.noreply.github.com>
2022-05-07 16:52:55 +0200

    Funcionan tooltips
    falta arreglar el texto que se vea mejor y la animación que ahora mismo no termina de ir bien (la transición)

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ d3.csv ("ev_mig.csv").then (function (datosCompletos){
     var width = 460 - margin.left - margin.right
     var height = 400 - margin.top - margin.bottom
     
+
     
     var elementoSVG = d3.select("body")
         .append("svg")
@@ -41,64 +42,7 @@ d3.csv ("ev_mig.csv").then (function (datosCompletos){
         .append("g")
             .attr("transform","translate(" + margin.left + "," + margin.top + ")");
     
-              //Tooltip
-
-    
-    var Tooltip = d3.select ("body")
-        .append("div")
-        .attr("class", "tooltip")
-        
-     var Tooltip2 = d3.select ("body")
-        .append("div")
-        .attr("class", "tooltip")
-        
-    
-    
-    //Función para el primer tooltip cuando el cursor se posiciona sobre un punto del gráfico dejándolo demarcado
-    var mouseover = function(d) {
-        Tooltip.style("opacity", 1)
-        d3.select(this)
-            .style("stroke", "black")
-            .style("opacity", 1)
-    }
-       var mouseover2 = function(d) {
-        Tooltip2.style("opacity", 1)
-        d3.select(this)
-            .style("stroke", "black")
-            .style("opacity", 1)
-    }
-    
-   //Función para el  tootltip cuando el cursor se posiciona sobre un punto del gráfico mostrando la información 
-    var mousemove = function(d){
-        Tooltip.html("<p style='color:#022920;'>Año: " + d.Año + "<br/> <b>" + d.Periodo + "</b>" + d.Emigración + "</p>")
-            .style("top", d3.event.pageY + 20 + "px")
-            .style("left", d3.event.pageX + 20 + "px")
-            .transition()
-            .style("opacity", 1)
-    }
-    
-     //  2  Función para el  tootltip cuando el cursor se posiciona sobre un punto del gráfico mostrando la información 
-    var mousemove2 = function(d){
-        Tooltip2.html("<p style='color:#022920;'>Año: " + d.Año + "<br/> <b>" + d.Periodo + "</b>" + d.Inmigración + "</p>")
-            .style("top", d3.event.pageY + 20 + "px")
-            .style("left", d3.event.pageX + 20 + "px")
-            .transition()
-            .style("opacity", 1)
-    }
-    
-    //Función para borrar el tooltip cuando el cursor se aleja
-    var mouseleave = function(d) {
-        Tooltip.style("opacity", 0)
-        d3.select(this)
-        .style("stroke", "none")
-        .style("opacity", 0.8)
-    }
-      var mouseleave2 = function(d) {
-        Tooltip2.style("opacity", 0)
-        d3.select(this)
-        .style("stroke", "none")
-        .style("opacity", 0.8)
-    }
+ 
       
         //EJE X
         var x = d3.scaleTime()
@@ -135,7 +79,7 @@ d3.csv ("ev_mig.csv").then (function (datosCompletos){
             .attr("stroke", "steelblue")
             .attr("stroke-width",1.5)
             .attr("d",linea_Emigracion)
-            
+
     
         elementoSVG.append("path")
             .datum(datos)
@@ -145,40 +89,108 @@ d3.csv ("ev_mig.csv").then (function (datosCompletos){
             .attr("d",linea_Inmigracion)
     
     
+    
+    
+                 //Tooltip
+
+    
+    var Tooltip = d3.select ("body")
+        .append("div")
+        .attr("class", "tooltip")
+        
+    var Tooltip2 = d3.select ("body")
+        .append("div")
+        .attr("class", "tooltip")
+        
+ 
+    
+    //Función para el primer tooltip cuando el cursor se posiciona sobre un punto del gráfico dejándolo demarcado
+    var mouseover = function(d) {
+        Tooltip.style("opacity", 1)
+        d3.select(this)
+            .style("stroke", "black")
+            .style("opacity", 1)
+    }
+       var mouseover2 = function(d) {
+        Tooltip2.style("opacity", 1)
+        d3.select(this)
+            .style("stroke", "black")
+            .style("opacity", 1)
+    }
+    
+   //Función para el  tootltip cuando el cursor se posiciona sobre un punto del gráfico mostrando la información 
+    var mousemove = function(d){
+        Tooltip.html("<p style='color:#022920;'>Año: " + d.Año + "<br/> <b>" + d.Periodo + "</b>" + d.Emigración + "</p>")
+            .style("top", d3.event.pageY + 20 + "px")
+            .style("left", d3.event.pageX + 20 + "px")
+            .transition()
+            .style("opacity", 1)
+    }
+    
+     //  2  Función para el  tootltip cuando el cursor se posiciona sobre un punto del gráfico mostrando la información 
+   var mousemove2 = function(d){
+        Tooltip2.html("<p style='color:#022920;'>Año: " + d.Año + "<br/> <b>" + d.Periodo + "</b>" + d.Inmigración + "</p>")
+            .style("top", d3.event.pageY + 20 + "px")
+            .style("left", d3.event.pageX + 20 + "px")
+            .transition()
+            .style("opacity", 1)
+    }
+    
+    //Función para borrar el tooltip cuando el cursor se aleja
+    var mouseleave = function(d) {
+        Tooltip.style("opacity", 0)
+        d3.select(this)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+    }
+      var mouseleave2 = function(d) {
+        Tooltip2.style("opacity", 0)
+        d3.select(this)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+    }
+      
+
+
     //pintar círculos en los valores de la linea 
-        elementoSVG.append("g")
+       var dots =  elementoSVG.append("g")
             .selectAll("dot")
             .data(datos)
             .enter()
             .append("circle")
-            .transition()
-            .duration(4000)
-            .ease(d3.easeBounce)
-            .delay(500)
             .attr ("r",4)
             .attr("cx", d=> x(new Date(d.Año, d.Periodo=="Semestre 1" ? 5 :  11, 30)))
             .attr("cy", d=> y(+d.Emigración))
-            .attr("fill","black")
-           /* .on("mouseover", mouseover)
+            .attr("fill","black")        
+    
+       dots.transition()
+            .duration(3000)
+            .ease(d3.easeBounce)
+            .delay(500)
+
+    
+        dots.on("mouseover", mouseover)
             .on("mousemove", d => { mousemove(d) })
-            .on("mouseleave", mouseleave)*/
+            .on("mouseleave", mouseleave)
          
-        elementoSVG.append("g")
+      var dots2 = elementoSVG.append("g")
             .selectAll("dot")
             .data(datos)
             .enter()
             .append("circle")
-            .transition()
-            .duration(4000)
-            .ease(d3.easeBounce)
-            .delay(500)
             .attr ("r",4)
             .attr("cx", d=> x(new Date(d.Año, d.Periodo=="Semestre 1" ? 5 :  11, 30)))
             .attr("cy", d=> y(+d.Inmigración))
             .attr("fill","brown")
-  /*          .on("mouseover", mouseover2)
+      
+      dots2.transition()
+            .duration(4000)
+            .ease(d3.easeBounce)
+            .delay(500)
+            
+       dots2.on("mouseover", mouseover2)
             .on("mousemove", d => { mousemove2(d) })
-            .on("mouseleave", mouseleave2)*/
+            .on("mouseleave", mouseleave2)
           
     
     })
